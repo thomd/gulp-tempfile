2230360636c263d36e19a85fd698d8c7f2119d5a
Thomas Duerr <thomduerr@gmail.com>
2015-02-23 23:55:37 +0100

    write a temporary file

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 'use strict';
-var util    = require('gulp-util');
+
 var through = require('through2');
+var path    = require('path');
+var tmpdir  = require('os').tmpdir();
+var uuid    = require('uuid');
+var mkdirp  = require('mkdirp');
+var fs      = require('fs');
+
+function tempfile(filepath) {
+  return path.join(tmpdir, uuid.v4(), (filepath || ''));
+}
 
 module.exports = function(options) {
+
   return through.obj(function(file, encoding, callback) {
-    // transform
-    callback(null, file); // noop
+    var abspath = tempfile(file.path);
+
+    var newFile = file.clone();
+    mkdirp(path.dirname(abspath), function (err) {
+      fs.writeFile(abspath, newFile.contents, function (err) {
+        newFile.path = abspath;
+        callback(err, newFile);
+      });
+    });
+
   }, function(callback) {
     // flush
   });
